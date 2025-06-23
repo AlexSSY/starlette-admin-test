ae53b96cb3425dd1c17025641f410d6010702022
AlexSSY <adolf5341@gmail.com>
2025-06-23 19:13:21 +0300

    manufactures

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from starlette.staticfiles import StaticFiles
 from starlette_admin.contrib.sqla import Admin, ModelView
 from starlette_admin.fields import PasswordField, EmailField
 from starlette_admin.exceptions import FormValidationError
+from starlette_admin import RequestAction
 
 from models import create_db, User
 from db import engine
@@ -33,26 +34,39 @@ admin = Admin(engine, title="Example: SQLAlchemy")
 
 
 class UserModelView(ModelView):
-    exclude_fields_from_create = ['created_at', 'updated_at']
+    exclude_fields_from_create = ['created_at', 'updated_at', 'password_hash']
+    exclude_fields_from_edit = ['created_at', 'updated_at', 'password_hash']
+    exclude_fields_from_list = ['password_hash']
     fields = [
-        EmailField('email', 'Email', required=True),
+        'id',
+        'email',
         PasswordField('password', 'Password', required=True, exclude_from_list=True, exclude_from_detail=True),
-        PasswordField('password_hash', "Hashed password", exclude_from_create=True, exclude_from_edit=True)
+        PasswordField('password_confirmation', 'Password Confirmation', required=True, exclude_from_list=True, exclude_from_detail=True),
+        'password_hash',
+        'created_at',
+        'updated_at'
     ]
 
     def validate(self, request, data):
         errors = dict()
+
+        # * ultra DB-UNIQUE
+        if (request.state.action == RequestAction.CREATE):
+            session = request.state.session
+            if session.query(self.model).where(self.model.email == data['email']).first():
+                errors['email'] = 'already exists'
+
         if len(data['password']) < 6:
             errors['password'] = 'too short, minimum 6 symbols'
+        if data['password'] != data['password_confirmation']:
+            errors['password_confirmation'] = 'password not match'
         if len(errors) > 0:
             raise FormValidationError(errors)
         return super().validate(request, data)
 
     def before_create(self, request, data, obj):
-        plain_password = data.pop('password')
-        password_hash = utils.hash_password(plain_password)
-        # data['password_hash'] = password_hash
-        obj.password_hash = password_hash
+        plain_password = data.get('password')
+        obj.password_hash = utils.hash_password(plain_password)
         return super().before_create(request, data, obj)
 
 
