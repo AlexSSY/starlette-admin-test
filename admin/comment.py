f173283e72aa9356b8f9993bf4d04a6be010e1f4
AlexSSY <adolf5341@gmail.com>
2025-06-25 20:01:33 +0300

    add validators to comments

--- admin/comment.py
+++ admin/comment.py
@@ -1,9 +1,15 @@
 from .admin_view import MyModelView
+from . import validators
 
 
 class CommentModelView(MyModelView):
     exclude_fields_from_create = ["created_at", "updated_at"]
     exclude_fields_from_edit = ["created_at", "updated_at"]
+    
+    create_validators = {
+        "post": [validators.required_validator()],
+        "author": [validators.required_validator()],
+    }
 
     async def repr(self, obj, request):
         return obj.text
